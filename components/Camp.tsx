7573dce49f52612a5a32d1034e52e963fa1fbe9c
Piotr Wrobel <wrobelpiotr24@gmail.com>
2023-11-02 23:40:41 +0100

    camp-area

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -23,12 +23,12 @@ const CampSite = ({ image, title, subtitle, people }: CampProps) => (
                     />
                 </div>
                 <div className='flex flex-col justify-center'>
-                    <p className="regular-menu text-white">{title}</p>
+                    <p className="regular-menu bold text-white">{title}</p>
                     <p className="regular-quotes text-white">{subtitle}</p>
                 </div>
             </div>
             <div className='flex flex-row'>
-                <div className='flex flex-row -space-x-4 overflow-hidden'>
+                <div className='flex flex-row items-center -space-x-4 overflow-hidden'>
                     {PEOPLE_URL.map((url) => (
                         <Image
                             src={url}
@@ -36,7 +36,7 @@ const CampSite = ({ image, title, subtitle, people }: CampProps) => (
                             alt='person'
                             width={52}
                             height={52}
-                            className='w-14'
+                            className='h-fit'
                         />
                     ))}
                 </div>
@@ -48,7 +48,7 @@ const CampSite = ({ image, title, subtitle, people }: CampProps) => (
 
 const Camp = () => {
     return (
-        <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+        <section className='max-container padding-container flex flex-col py-10 lg:py-20'>
             <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-10 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
                 <CampSite
                     image="bg-bg-img-1"
@@ -57,13 +57,26 @@ const Camp = () => {
                     people="50+ Joined"
                 />
                 <CampSite
-                    image="bg-bg-img-1"
-                    title="Putuk Truno Camp"
-                    subtitle="Prigen, Pasuruan"
-                    people="50+ Joined"
+                    image="bg-bg-img-2"
+                    title="Mountain View Camp"
+                    subtitle="Somewhere in the Wildness"
+                    people="100+ Joined"
                 />
             </div>
-        </section>
+            <div className="flexEnd mt-10 lg:-mt-60 lg:mr-6">
+                <div className='bg-green-50 p-10 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl'>
+                    <h2 className="regular text-white mb-4 capitalize"><strong className='bold'>Feeling Lost</strong> And Not Knowing The Way?</h2>
+                    <p className="regular-menu text-white">Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure</p>
+                    <Image
+                        src="/quote.svg"
+                        alt="camp-2"
+                        width={186}
+                        height={219}
+                        className='camp-quote'
+                    />
+                </div>
+            </div>
+        </section >
     )
 }
 
