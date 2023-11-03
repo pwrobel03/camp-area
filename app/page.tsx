2b01d7df806c7eaae463d26645b5f50634ec4ae9
Piotr Wrobel <wrobelpiotr24@gmail.com>
2023-11-03 01:15:13 +0100

    features

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Hero from '@/components/Hero'
 import Image from 'next/image'
 import Camp from '@/components/Camp'
 import Guide from '@/components/Guide'
+import Features from '@/components/Features'
 
 export default function Home() {
   return (
     <>
-      <Hero></Hero>
-      <Camp></Camp>
-      <Guide></Guide>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
     </>
   )
 }
---
+++ components/Features.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Image from 'next/image'
+import { FEATURES } from '@/constants'
+
+
+const Features = () => {
+    return (
+        <section className="max-container padding-container relative bg-feature-bg bg-no-repeat bg-center flex flex-row py-10 lg:py-20">
+            <div className="flex flex-1 lg:min-h-[1100px]">
+                <Image
+                    src="/phone.png"
+                    alt='phone'
+                    width={440}
+                    height={1000}
+                    className='feature-phone z-[1]' />
+            </div>
+            <div className="flex w-full flex-col z-[2] gap-6 lg:w-2/3">
+                <div>
+                    <Image
+                        src="/camp.svg"
+                        alt="camp"
+                        width={50}
+                        height={50}
+                        className="float-left mr-5 w-10 lg:w-[80px]"
+                    />
+                    <h1 className="headings capitalize">our features</h1>
+                </div>
+                <ul className='grid md:grid-cols-2 gap-10'>
+                    {FEATURES.map(feature => (
+                        <li className='flex flex-col '>
+                            <div className='flex sm:flex-col flex-row items-center gap-2 '>
+                                <div className='flex max-w-[64px] bg-green-50 p-5 rounded-full'>
+                                    <Image
+                                        src={feature.icon}
+                                        alt={feature.title}
+                                        width={28}
+                                        height={28}
+                                    />
+                                </div>
+                                <h2 className="regular bold">
+                                    {feature.title}
+                                </h2>
+                            </div>
+                            <p className="regular mt-2 text-gray-30">
+                                {feature.description}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Features
